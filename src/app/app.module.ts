642125d15208393687bd052cd102da2ef06d94d5
Alexandru Iliescu <alexandru.iliescu26@gmail.com>
2018-10-28 21:22:26 +0200

    Some new thing on home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import {MatDividerModule} from '@angular/material';
 import { HeaderAcComponent } from './components/header-ac/header-ac.component';
+import { ListComponent } from './components/list/list.component';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderAcComponent
+    HeaderAcComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatDividerModule
+    MatDividerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
